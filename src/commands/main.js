d161982d55774f1e1ef86ca45abe228ba9de3e4c
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-12-05 13:52:42 +0530

    Update main.js

--- src/commands/main.js
+++ src/commands/main.js
@@ -1,1 +1,35 @@
+const os = require('os')
+const {runtime} = require("../utils/functions")
+module.exports = [
+  {
+    name:"alive",
+    description:"Bot online check",
+    react:"🤖",
+    ownerOnly: false,
+    gruopOnly: false,
+    adminOnly: false,
+    botAdmin: false,
+    async execute(conn,mek,args,context){
+      const {from,pushname,reply,quoted} = context;
+      try{
+        let desc = `
+Hey ${pushname} I am Alive now🌝💗
+uptime:${runtime(process.uptime())}
 
+> ZANTA-MD-V2
+`
+await conn.sendMessage(from,
+     {
+        image:{url:""},
+        caption: desc
+     },{
+       quoted:mek
+     }
+    )        
+        
+      }catch(e){
+        console.log(e)
+      }
+    }
+  }
+]
